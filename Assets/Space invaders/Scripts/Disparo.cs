9da522f589c255405fbac0200700c7e22a4bdfa1
Kim Baltrons Mata <quimbaltrons@gmail.com>
2026-01-18 21:05:40 +0100

    explotion

--- Assets/Space invaders/Scripts/Disparo.cs
+++ Assets/Space invaders/Scripts/Disparo.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class Disparo : MonoBehaviour
 {
     public float moveSpeed;
+    public GameObject explosionPrefab;
     void Start()
     {
         
@@ -14,8 +15,10 @@ public class Disparo : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
+       
         if (collision.gameObject.tag == "Enemigo")
         {
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
             Destroy(collision.gameObject);
             Destroy(gameObject);
         }
