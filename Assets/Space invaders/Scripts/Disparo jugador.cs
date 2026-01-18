3e95c4a6eac77d72ece49c7a8422ad9a8e00911a
Kim Baltrons Mata <quimbaltrons@gmail.com>
2026-01-18 16:20:56 +0100

    cambio a jump

--- Assets/Space invaders/Scripts/Disparo jugador.cs
+++ Assets/Space invaders/Scripts/Disparo jugador.cs
@@ -10,7 +10,7 @@ public class Disparojugador : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")) 
+        if (Input.GetButtonDown("Jump")) 
 
         {
             Instantiate(disparoPrefab, transform.position, Quaternion.identity) ;
