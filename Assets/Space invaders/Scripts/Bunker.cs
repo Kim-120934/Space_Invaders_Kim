ae6f3743ddf2760d569646be1fae43504df11edc
Kim Baltrons Mata <quimbaltrons@gmail.com>
2026-01-20 23:49:16 +0100

    bunker

---
+++ Assets/Space invaders/Scripts/Bunker.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Bunker : MonoBehaviour
+{
+
+    public Sprite[] damageStages; // Array de sprites con diferentes niveles de daño
+    public int health = 4; // Debe coincidir con el número de sprites
+    public GameObject explosionPrefab;
+
+    private SpriteRenderer spriteRenderer;
+    private int currentStage = 0;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Disparo" || collision.gameObject.tag == "Enemigo Disparo")
+        {
+            health--;
+
+            if (explosionPrefab != null)
+            {
+                Instantiate(explosionPrefab, collision.transform.position, Quaternion.identity);
+            }
+
+            Destroy(collision.gameObject);
+
+            // Cambia el sprite según el daño
+            if (health > 0 && currentStage < damageStages.Length)
+            {
+                spriteRenderer.sprite = damageStages[currentStage];
+                currentStage++;
+            }
+            else if (health <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
+
